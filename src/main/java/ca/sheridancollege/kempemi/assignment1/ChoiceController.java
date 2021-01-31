b871e5d679e7801ebc403854184c13343fea3586
Michael <you@example.com>
2021-01-30 22:10:25 -0500

    Added basic functionality for program. Index.html, ChoiceController, and myCss were created. Form and immediate mappings created.

---
+++ src/main/java/ca/sheridancollege/kempemi/assignment1/ChoiceController.java
@@ -0,0 +1,19 @@
+package ca.sheridancollege.kempemi.assignment1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ChoiceController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @PostMapping ("/determineWinner")
+    public String determineResults (){
+        return "/";
+    }
+}
