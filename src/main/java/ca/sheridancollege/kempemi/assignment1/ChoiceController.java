c71f84b260e5cae13462823c8e7733c8776e745a
Michael <you@example.com>
2021-01-31 15:55:09 -0500

    Set up model to pass results.

--- src/main/java/ca/sheridancollege/kempemi/assignment1/ChoiceController.java
+++ src/main/java/ca/sheridancollege/kempemi/assignment1/ChoiceController.java
@@ -1,8 +1,10 @@
 package ca.sheridancollege.kempemi.assignment1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ChoiceController {
@@ -13,7 +15,12 @@ public class ChoiceController {
     }
 
     @PostMapping ("determineWinner")
-    public String determineResults (){
+    public String determineResults(Model model, @RequestParam("gesture") String gesture){
+
+        model.addAttribute("player", gesture);
+        model.addAttribute("computer", "unkown");
+        model.addAttribute("result", "unkown");
+
         return "Results";
     }
 }
